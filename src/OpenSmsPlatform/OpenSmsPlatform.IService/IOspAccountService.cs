1c643d60413edb5861a159e81302fbc9a796086b
chusj <chusj.mail@qq.com>
2024-08-23 17:48:12 +0800

    添加账号服务，2个方法：验证账号、查询账号

---
+++ src/OpenSmsPlatform/OpenSmsPlatform.IService/IOspAccountService.cs
@@ -0,0 +1,24 @@
+﻿using OpenSmsPlatform.Model;
+
+namespace OpenSmsPlatform.IService
+{
+    public interface IOspAccountService
+    {
+        /// <summary>
+        /// 查询账号
+        /// </summary>
+        /// <param name="accId">账号id</param>
+        /// <param name="smsSuffix">短信签名</param>
+        /// <returns></returns>
+        Task<OspAccount> QueryOspAcount(string accId, string smsSuffix);
+
+        /// <summary>
+        /// 验证账号
+        /// </summary>
+        /// <param name="accId">账号id</param>
+        /// <param name="timeStamp">时间戳</param>
+        /// <param name="signature">签名</param>
+        /// <returns></returns>
+        public Task<bool> ValidAccount(string accId, string timeStamp, string signature);
+    }
+}
---
+++ src/OpenSmsPlatform/OpenSmsPlatform.Service/OspAccountService.cs
@@ -0,0 +1,63 @@
+﻿using OpenSmsPlatform.Common;
+using OpenSmsPlatform.Common.Helper;
+using OpenSmsPlatform.IService;
+using OpenSmsPlatform.Model;
+using OpenSmsPlatform.Repository;
+using SqlSugar;
+
+namespace OpenSmsPlatform.Service
+{
+    public class OspAccountService : IOspAccountService
+    {
+        private readonly IBaseRepository<OspAccount> _accountRepository;
+
+        public OspAccountService(IBaseRepository<OspAccount> accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<bool> ValidAccount(string accId, string timeStamp, string signature)
+        {
+            //1.计算时间戳差值
+            long serverST = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            long timeSpan = serverST - timeStamp.ObjToLong();
+
+            if (timeSpan > 180 || timeSpan < -180)
+            {
+                return false;
+            }
+
+            //2.根据accid查询账号信息
+            OspAccount account = await _accountRepository.QueryById(accId);
+            if (account != null)
+            {
+                // 构建签名字符串
+                string signString = $"AccId={accId}" +
+                                    $"&AccKey={account.AccKey}" +
+                                    $"&AccSecret={account.AccSecret}" +
+                                    $"&SmfSuffix={account.SmsSuffix}" +
+                                    $"&TimeStamp={timeStamp}";
+
+                if (signature == Md5Helper.EncryptMD5(signString))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public async Task<OspAccount> QueryOspAcount(string accId, string smsSuffix)
+        {
+            var condition = Expressionable.Create<OspAccount>();
+            condition.And(x => x.AccId == accId && x.SmsSuffix == smsSuffix);
+
+            List<OspAccount> accountList = await _accountRepository.Query(condition.ToExpression());
+            if (accountList.Count() == 1)
+            {
+                return accountList[0];
+            }
+            else { return null; }
+        }
+    }
+}
