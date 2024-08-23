8cd506b2e9b68275457f038a692a72f2f9d658c5
chusj <chusj.mail@qq.com>
2024-08-23 17:48:45 +0800

    新增api请求实体、短信发送实体

---
+++ src/OpenSmsPlatform/OpenSmsPlatform.Model/http/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenSmsPlatform.Model
+{
+    /// <summary>
+    /// Api响应
+    /// </summary>
+    public class ApiResponse
+    {
+        public int Code { get; set; }  = 400;
+
+        /// <summary>
+        /// 信息
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ src/OpenSmsPlatform/OpenSmsPlatform.Model/http/SmsRequest.cs
@@ -0,0 +1,43 @@
+﻿namespace OpenSmsPlatform.Model
+{
+    /// <summary>
+    /// 短信请求对象
+    /// </summary>
+    public class SmsRequest
+    {
+        /// <summary>
+		/// 手机号码
+		/// </summary>
+		public List<string> Mobiles { get; set; }
+
+        /// <summary>
+        /// 短信内容
+        /// </summary>
+        public string Contents { get; set; }
+
+        /// <summary>
+        /// 手机验证码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 短信签名
+        /// </summary>
+        public string SmsSuffix { get; set; }
+
+        /// <summary>
+        /// 账号id
+        /// </summary>
+        public string AccId { get; set; }
+
+        /// <summary>
+        /// 时间戳
+        /// </summary>
+        public string TimeStamp { get; set; }
+
+        /// <summary>
+        /// 签名
+        /// </summary>
+        public string Signature { get; set; }
+    }
+}
