9bf405692be09e1d43724a6235767fb4242748de
chusj <chusj.mail@qq.com>
2024-08-24 17:07:28 +0800

    增加OspRecord服务接口和服务类

---
+++ src/OpenSmsPlatform/OpenSmsPlatform.IService/IOspRecordService.cs
@@ -0,0 +1,15 @@
+﻿using OpenSmsPlatform.Model;
+
+namespace OpenSmsPlatform.IService
+{
+    public interface IOspRecordService
+    {
+        /// <summary>
+        /// 添加记录并更新余额
+        /// </summary>
+        /// <param name="recordList">短信记录</param>
+        /// <param name="account">账号实体</param>
+        /// <returns></returns>
+        Task<bool> AddRecordsAndUpdateAmount(List<OspRecord> recordList, OspAccount account);
+    }
+}
---
+++ src/OpenSmsPlatform/OpenSmsPlatform.Service/OspRecordService.cs
@@ -0,0 +1,36 @@
+﻿using OpenSmsPlatform.Common.Transcation;
+using OpenSmsPlatform.IService;
+using OpenSmsPlatform.Model;
+using OpenSmsPlatform.Repository;
+
+namespace OpenSmsPlatform.Service
+{
+    public class OspRecordService : IOspRecordService
+    {
+        private readonly IBaseRepository<OspAccount> _accountRepository;
+        private readonly IBaseRepository<OspRecord> _recordRepository;
+
+        public OspRecordService(
+            IBaseRepository<OspAccount> accountRepository,
+            IBaseRepository<OspRecord> recordRepository)
+        {
+            _accountRepository = accountRepository;
+            _recordRepository = recordRepository;
+        }
+
+
+        [UseTran(Propagation = Propagation.Required)]
+        public async Task<bool> AddRecordsAndUpdateAmount(List<OspRecord> recordList, OspAccount account)
+        {
+            int i = await _recordRepository.AddSplit(recordList);
+
+            return await UpdateAccountAmount(account);
+        }
+
+        [UseTran(Propagation = Propagation.Required)]
+        private async Task<bool> UpdateAccountAmount(OspAccount account)
+        {
+            return await _accountRepository.Update(account);
+        }
+    }
+}
