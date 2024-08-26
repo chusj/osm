a08da9090d59be50581004d4af4e586b74ffa204
chusj <chusj.mail@qq.com>
2024-08-26 11:48:11 +0800

    增加Limit服务接口和服务类

---
+++ src/OpenSmsPlatform/OpenSmsPlatform.Service/OspLimitService.cs
@@ -0,0 +1,35 @@
+﻿using OpenSmsPlatform.IService;
+using OpenSmsPlatform.Model;
+using OpenSmsPlatform.Repository;
+using SqlSugar;
+
+namespace OpenSmsPlatform.Service
+{
+    public class OspLimitService : IOspLimitService
+    {
+        private readonly IBaseRepository<OspLimit> _limitRepository;
+
+        public OspLimitService(IBaseRepository<OspLimit> limitRepository)
+        {
+            _limitRepository = limitRepository;
+        }
+
+        /// <summary>
+        /// 是否在限制名单中
+        /// </summary>
+        /// <param name="mobile"></param>
+        /// <returns></returns>
+        public async Task<OspLimit> IsInLimitList(string mobile)
+        {
+            var condition = Expressionable.Create<OspLimit>();
+            condition.And(x => x.Mobile == mobile && x.LimitType == 2);
+
+            List<OspLimit> limitList = await _limitRepository.Query(condition.ToExpression());
+            if (limitList.Count() == 1)
+            {
+                return limitList[0];
+            }
+            else { return null; }
+        }
+    }
+}
